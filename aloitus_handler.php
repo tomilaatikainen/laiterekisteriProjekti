0ea737c57c2ca4b3d41074243731a8ed8c954adb
Lauri K <44801633+laurikunnap@users.noreply.github.com>
2018-11-13 13:36:12 +0200

    Asiakkaan tarkistus
    Asiakkaan tarkistamisen aloitus

--- aloitus_handler.php
+++ aloitus_handler.php
@@ -1,13 +1,27 @@
 <?php
 	session_start();
+	require_once("asiakas_utils.inc");
 	require_once("getpost.inc");
 		$asiakas["tunnus"] = parsePost("tunnus");
 		$asiakas["salasana"] = parsePost("ss");
 		$asiakas["nimi"] = parsePost("nimi");
 
-		$_SESSION["login"] = 1;
+		tarkistaAsiakas($asiakas);
+
+		if($result)
+		{
 		
+		$_SESSION["login"] = 1;
 		header("Location: kayttaja.php");
 		exit();
+		}
+
+		else
+		{
+		
+		}
+		
+
+		
 	
 ?>
