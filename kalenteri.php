1eb2fe269818403d56e0a3354fe5c6eb2f646061
tomilaatikainen <tomi.laatikainen@edu.savonia.fi>
2018-12-04 12:38:09 +0200

    kalenteri.php ja css kikkailua
    kalenteri.php ja css kikkailua

---
+++ kalenteri.php
@@ -0,0 +1,63 @@
+<?php
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Uusi varaus</title>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" href="/resources/demos/style.css">
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <style>
+        
+    </style>
+	
+	  <script>
+  $(document).ready(function () {
+  
+  $( function() {
+	  
+	  $.datepicker.setDefaults($.datepicker.regional['fi'] = {
+                    closeText: "Sulje",
+                    prevText: "&#xAB;Edellinen",
+                    nextText: "Seuraava&#xBB;",
+                    currentText: "Tänään",
+                    monthNames: ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu",
+                        "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"],
+                    monthNamesShort: ["Tammi", "Helmi", "Maalis", "Huhti", "Touko", "Kesä",
+                        "Heinä", "Elo", "Syys", "Loka", "Marras", "Joulu"],
+                    dayNamesShort: ["Su", "Ma", "Ti", "Ke", "To", "Pe", "La"],
+                    dayNames: ["Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"],
+                    dayNamesMin: ["Su", "Ma", "Ti", "Ke", "To", "Pe", "La"],
+                    weekHeader: "Vk",
+                    dateFormat: "d.m.yy",
+                    firstDay: 1,
+                    isRTL: false,
+                    showMonthAfterYear: false,
+                    yearSuffix: ""
+                });
+				
+    $( "#alkupvm" ).datepicker({
+		minDate: 0,
+	});
+	
+	$( "#loppupvm" ).datepicker({
+		minDate: 0,
+	});
+	} );
+	
+	
+	
+	
+  });
+  </script>
+	
+</head>
+<body>	
+	<p>Alkupvm: <input type="text" id="alkupvm"></p>
+	<p>Loppupvm: <input type="text" id="loppupvm"></p>
+</body>
+</html>
--- muokkaa.php
+++ muokkaa.php
@@ -13,6 +13,36 @@
     <title>Muokkaa käyttäjätietoja</title>
     <style>
         
+        #form_login {
+            border: 3px solid black;
+			border-radius: 20px;
+            width: 500px;
+            height: 500px;
+        }
+
+        #div_login {
+            margin-left: 20px;
+			margin-right: 20px;
+        }
+		
+		input[type=text], input[type=password] {
+		width: 100%;
+		padding: 12px 20px;
+		margin: 8px 0;
+		display: inline-block;
+		border: 1px solid #ccc;
+		box-sizing: border-box;
+		}
+		
+		input[type=submit] {
+		background-color: #4CAF50;
+		color: white;
+		padding: 14px 20px;
+		margin: 8px 0;
+		border: none;
+		cursor: pointer;
+		width: 100%;
+		}
     </style>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -35,7 +65,7 @@
 </head>
 <body>	
 	<form id="form_login" action="muokkaa_handler.php" method="post">
-	<div>
+	<div id="div_login">
         <h1>Muokkaa tietoja</h1>
 
         Uusi salasana: <input type="password" name="uusiSalasana" value="<?php echo $us; ?>" /><br />
